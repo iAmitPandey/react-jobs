23d381117095254165bb20a06fba7ee526969ed2
Amit Pandey <amitpandey21599@gmail.com>
2024-10-19 22:49:33 +0530

    Start the React job project

---
+++ src/components/JobListings.jsx
@@ -0,0 +1,40 @@
+import { useState, useEffect } from "react";
+import JobListing from "./JobListing";
+import { data } from "autoprefixer";
+
+const JobListings = ({ isHome = false }) => {
+  const [jobs, setJobs] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    try {
+      const fetchJobs = async () => {
+        const res = await fetch("http://localhost:8000/jobs");
+        const data = await res.json();
+        setJobs(data);
+      };
+    } catch (err) {
+      console.log(err);
+    } finally {
+      setLoading(false);
+    }
+    fetchJobs();
+  }, []);
+
+  return (
+    <section className="bg-blue-50 px-4 py-10">
+      <div className="container-xl lg:container m-auto">
+        <h2 className="text-3xl font-bold text-indigo-500 mb-6 text-center">
+          {isHome ? "Recent Jobs" : "Browse Jobs"}
+        </h2>
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+          {jobs.map((job) => {
+            return <JobListing key={job.id} job={job} />;
+          })}
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default JobListings;
